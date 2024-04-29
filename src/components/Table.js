5388299c97bd13657976eff51c3d44e794e1290b
BharkaviSandhiya <bmohan.sandhiya@nutechnologyinc.com>
2024-04-29 16:02:56 +0530

    Redux_Toolkit

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers, deleteUser, updateUser } from '../../src/redux/userSlice';
 import './Table.css';
